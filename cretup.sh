c3014b5f69e79c40eb7abfdefd504587141de98e
Angus Alves <angustalves@gmail.com>
2017-09-26 21:59:30 -0400

    initial commit

---
+++ cretup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget -q -O - https://raw.github.com/skycocker/chrombrew/master/install.sh | bash
+crew install buildessential
+crew install make
+crew install vim
+crew install gdrive
+crew install aria2
+crew install attr
+crew install cmatrix
+crew install curl
+crew install gnupg
