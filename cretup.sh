8d94fad277a98eaa792802aaf01e486f7ac70787
Angus Alves <angustalves@gmail.com>
2017-09-27 23:55:01 -0400

    fixed vim plugin install so runs in background

--- cretup.sh
+++ cretup.sh
@@ -10,7 +10,8 @@ crew install cmatrix
 crew install curl
 crew install gnupg
 curl 'http://vim-bootstrap.com/generate.vim' --data 'langs=javascript&langs=php&langs=html&langs=ruby&langs=python&editor=vim' > ~/.vimrc
-exec "vim +PlugInstall +qall"
+vim +PlugInstall +qall &
+wait
 curl 'https://goo.gl/fd3zc' > ~/Downloads/crouton
 sudo chromeos-setdevpasswd
 echo "sudo sh -e ~/Downloads/crouton -e -n kali -p /media/removeable/k/ -r kali-rolling -t audio,cli-extra,core,extension,xfce"
