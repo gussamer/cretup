ae51e937e70af611c22515d1fdb69a1397f561c2
Angus Alves <angustalves@gmail.com>
2017-09-27 23:43:47 -0400

    fixed vim plugin install

--- cretup.sh
+++ cretup.sh
@@ -10,7 +10,7 @@ crew install cmatrix
 crew install curl
 crew install gnupg
 curl 'http://vim-bootstrap.com/generate.vim' --data 'langs=javascript&langs=php&langs=html&langs=ruby&langs=python&editor=vim' > ~/.vimrc
-vim +PlugInstall +qall
+vim - +PlugInstall +qall
 curl 'https://goo.gl/fd3zc' > ~/Downloads/crouton
 sudo chromeos-setdevpasswd
 echo "sudo sh -e ~/Downloads/crouton -e -n kali -p /media/removeable/k/ -r kali-rolling -t audio,cli-extra,core,extension,xfce"
