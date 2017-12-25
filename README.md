7c709df79e3f3b298f461586257f013437d3ade5
Angus Alves <angustalves@gmail.com>
2017-12-25 17:37:35 -0500

    fixed mid script sudo and dead terminal on completion

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-Cretup: Chrome OS Setup
+#Cretup
+##Chrome OS Setup
 A script I use to setup my chromebook dev machine after a powerwash.
-
-
+##Usage
+```
 wget -q -O - https://raw.github.com/gussamer/cretup/master/cretup.sh | bash
+```
+##Scripts
+* requires root privileges
+- fcard: fixes sd card being mounted as read only*
+- ikal: install a basic kali chroot with no gui*
+- iubu: install a ubuntu chroot with gnome*
+- la: ls -a
+- ll: ll -l
+- startk: starts the kali chroot created by ikal*
+- startu: starts the ubunutu chroot created by iubu*
+- upcrouton: updates the crouton script to latest version
--- cretup.sh
+++ cretup.sh
@@ -1,4 +1,18 @@
-#!/bin/bash
+#!/bin/sh -e
+wget -q -O - https://raw.github.com/dnschneid/crouton/master/installer/crouton > /tmp/crouton &
+wait
+chmod +x /tmp/crouton
+stty -echo
+printf "Password: "
+read pass < /dev/tty
+stty echo
+printf "\n"
+echo $pass | sudo -S mv -f /tmp/crouton /usr/local/bin/
+cd /tmp/
+git clone https://github.com/gussamer/cretup.git
+cp -fu ./cretup/scripts/* /usr/local/bin/
+cd ~
+rm -frd /tmp/cretup
 wget -q -O - https://raw.github.com/skycocker/chrombrew/master/install.sh | bash
 crew install buildessential
 crew install make
@@ -10,14 +24,4 @@ crew install cmatrix
 crew install curl
 crew install gnupg
 curl 'http://vim-bootstrap.com/generate.vim' --data 'langs=javascript&langs=php&langs=html&langs=ruby&langs=python&editor=vim' > ~/.vimrc
-vim +PlugInstall +qall &
-wait
-wget -q -O - https://raw.github.com/dnschneid/crouton/master/installer/crouton > ~/Downloads/crouton &
-wait
-mkdir ~/temp
-cd ~/temp
-git clone https://github.com/gussamer/cretup.git
-cp -fu ./cretup/scripts/* /usr/local/bin/
-cd ~
-rm -frd ~/temp
-cmatrix
+vim +PlugInstall +qall
